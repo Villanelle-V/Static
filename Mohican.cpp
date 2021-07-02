6d4dfcb79e754d0c2f06b80667793940bc63aee7
Polina <82818489+Villanelle-V@users.noreply.github.com>
2021-07-02 23:48:02 +0300

    Update Mohican.cpp

--- Mohican.cpp
+++ Mohican.cpp
@@ -1,39 +1,26 @@
-//#include "Mohican.h"
-//
-//Mohican::Mohican() {
-//	count += 1;
-//	this->id = count;
-//
-//	next = nullptr;
-//	prev = last;
-//	last = this;
-//
-//	list.push_back(this);
-//}
-//
-//Mohican::~Mohican() {
-//	prev = this;
-//	last = nullptr;
-//
-//	//list.erase(this);
-//}
-//
-//int Mohican::getId() {
-//	return this->id;
-//}
-//
-////const Mohican& Mohican::getLast() {
-////	return *this;
-////}
-//
-//void Mohican::getLast() {
-//	std::cout << list.back();
-//}
-//
-//int Mohican::count = 0;
-//Mohican* Mohican::last = nullptr;
-//
-////std::ostream& operator<<(std::ostream& out, const Mohican& mohican) {
-////	out << "Last Mohican is " << mohican.getId();
-////	return out;
-////}
+#include "Mohican.h"
+
+Mohican::Mohican() {
+	count += 1;
+	this->id = count;
+
+	list.push_back(this);
+	last = list.back();
+}
+
+Mohican::~Mohican() {
+	list.pop_back();
+	last = list.back();
+}
+
+int Mohican::getId() {
+	return this->id;
+}
+
+Mohican& Mohican::getLast() {
+	return *last;
+}
+
+int Mohican::count = 0;
+Mohican* Mohican::last = nullptr;
+std::list<Mohican*>Mohican::list = std::list<Mohican*>();
